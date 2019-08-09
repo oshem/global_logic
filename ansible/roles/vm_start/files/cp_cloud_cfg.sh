d82941a334e19f513d87d73ab8b6385cde52e269
oshem <aleks.personalemail@gmail.com>
2019-08-09 17:40:11 +0300

    fix

--- ansible/roles/vm_start/files/cp_cloud_cfg.sh
+++ ansible/roles/vm_start/files/cp_cloud_cfg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
+echo "Copy cloud_config"
 cp ./config_vm1.iso var/lib/libvirt/images/global_logic/
-cp ./cp ./config_vm2.iso var/lib/libvirt/images/global_logic/
+cp ./config_vm2.iso var/lib/libvirt/images/global_logic/
