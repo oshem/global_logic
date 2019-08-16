e9a097d1168c91568b23d45f0ec1f0a6f3dd3ec6
oshem <aleks.personalemail@gmail.com>
2019-08-16 18:24:24 +0300

    temp commit

--- ansible/roles/vm_start/files/download_image.sh
+++ ansible/roles/vm_start/files/download_image.sh
@@ -7,18 +7,26 @@ function copy_image() {
    echo -e "Base image does exist";
     if [ -d $libvirt_path/global_logic ];
     then
-       if [ -f $libvirt_path/$bs_img ];
+       if [ -f $libvirt_path/$bs_img_1 ];
         then
           echo -e "Image for KVM does exist"
          else
-         sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img
+         sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_1
+        echo -e "File has been created"
+        fi
+        if [ -f $libvirt_path/$bs_img_2 ];
+        then
+          echo -e "Image for KVM does exist"
+         else
+         sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_2
         echo -e "File has been created"
         fi
     else
       echo "Folder with image don't exist"
     sudo mkdir $libvirt_path/global_logic/ ;
      echo "Folder was  created"
-     sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img;
+     sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_1;
+     sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_2;
      echo "Image has been copied"
 fi
 }
@@ -27,7 +35,8 @@ function prepare_new_image() {
    sudo cd $libvirt_path && wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img ;
    sudo qemu-img resize bionic-server-cloudimg-amd64.img 10G
    sudo mkdir $libvirt_path/global_logic/ ;
-   sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/bs_img ;
+   sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_1 ;
+    sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_2;
 }
 
 
