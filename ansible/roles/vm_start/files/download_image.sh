ac857744e453ed395220fc945676c61aa0924cc2
oshem <aleks.personalemail@gmail.com>
2019-08-16 16:50:55 +0300

    temporarry added sudo to scripts

--- ansible/roles/vm_start/files/download_image.sh
+++ ansible/roles/vm_start/files/download_image.sh
@@ -2,6 +2,7 @@
 libvirt_path=/var/lib/libvirt/images
 bs_img=global_logic/base_image.qcow2
 
+
 function copy_image() {
    echo -e "Base image does exist";
     if [ -d $libvirt_path/global_logic ];
@@ -10,31 +11,36 @@ function copy_image() {
         then
           echo -e "Image for KVM does exist"
          else
-        cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img
+         sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img
         echo -e "File has been created"
         fi
     else
       echo "Folder with image don't exist"
-     mkdir $libvirt_path/global_logic/ ;
+    sudo mkdir $libvirt_path/global_logic/ ;
      echo "Folder was  created"
-     cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img;
+     sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img;
      echo "Image has been copied"
 fi
 }
 
 function prepare_new_image() {
-   cd $libvirt_path && wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img ;
-   qemu-img resize bionic-server-cloudimg-amd64.img 10G
-   mkdir $libvirt_path/global_logic/ ;
-   cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/bs_img ;
+   sudo cd $libvirt_path && wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img ;
+   sudo qemu-img resize bionic-server-cloudimg-amd64.img 10G
+   sudo mkdir $libvirt_path/global_logic/ ;
+   sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/bs_img ;
 }
 
 
 
+function chck_cp() {
 if [ -f $libvirt_path/bionic-server-cloudimg-amd64.img ];
  then
-   copy_image;
+    copy_image;
  else
    prepare_new_image;
 fi
+}
+
+chck_cp;
+
 
