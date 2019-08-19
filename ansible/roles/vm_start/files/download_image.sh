3dc0d9bdbf4b4b0aaa143c9d45aa3d2c5f356ea8
oshem <aleks.personalemail@gmail.com>
2019-08-19 11:50:04 +0300

    fix script 'prepare images'

--- ansible/roles/vm_start/files/download_image.sh
+++ ansible/roles/vm_start/files/download_image.sh
@@ -1,48 +1,60 @@
 #!/usr/bin/env bash
-libvirt_path=/var/lib/libvirt/images
-bs_img=global_logic/base_image.qcow2
 
+
+#Env
+
+libvirt_path=/var/lib/libvirt/images
+bs_img=bionic-server-cloudimg-amd64.img
+vm1=/var/lib/libvirt/images/global_logic/vm1
+vm2=/var/lib/libvirt/images/global_logic/vm2
 
 function copy_image() {
    echo -e "Base image does exist";
     if [ -d $libvirt_path/global_logic ];
     then
-       if [ -f $libvirt_path/$bs_img_1 ];
+
+       if [ -f $vm1 ];
         then
           echo -e "Image for KVM does exist"
          else
-         sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_1
+         cp $libvirt_path/$bs_img $vm1
         echo -e "File has been created"
         fi
-        if [ -f $libvirt_path/$bs_img_2 ];
+
+        if [ -f vm2 ];
         then
           echo -e "Image for KVM does exist"
          else
-         sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_2
+         sudo cp $libvirt_path/$bs_img $vm2
         echo -e "File has been created"
         fi
+
     else
-      echo "Folder with image don't exist"
-    sudo mkdir $libvirt_path/global_logic/ ;
+
+     echo "Folder with image don't exist"
+     mkdir $libvirt_path/global_logic/ ;
      echo "Folder was  created"
-     sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_1;
-     sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_2;
+
+     cp $libvirt_path/$bs_img $vm1;
+     cp $libvirt_path/$bs_img $vm2;
+
      echo "Image has been copied"
 fi
 }
 
 function prepare_new_image() {
-   sudo cd $libvirt_path && wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img ;
-   sudo qemu-img resize bionic-server-cloudimg-amd64.img 10G
-   sudo mkdir $libvirt_path/global_logic/ ;
-   sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_1 ;
-    sudo cp $libvirt_path/bionic-server-cloudimg-amd64.img $libvirt_path/$bs_img_2;
+
+    cd $libvirt_path && wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img ;
+    qemu-img resize bionic-server-cloudimg-amd64.img 10G
+    mkdir -p $libvirt_path/global_logic/ ;
+    cp $libvirt_path/$bs_img $vm1 ;
+    cp $libvirt_path/$bs_img $vm2;
 }
 
 
 
 function chck_cp() {
-if [ -f $libvirt_path/bionic-server-cloudimg-amd64.img ];
+if [ -f $libvirt_path/$bs_img ];
  then
     copy_image;
  else
