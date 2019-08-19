81f77317367bd34f04eed0c3074bf5c9bef77fb2
oshem <aleks.personalemail@gmail.com>
2019-08-19 15:09:02 +0300

    rewrite tasks

--- ansible/roles/vm_start/files/vm1.sh
+++ ansible/roles/vm_start/files/vm1.sh
@@ -3,7 +3,7 @@ virt-install \
   --memory 2048 \
   --vcpus 2 \
   --name worker1 \
-  --disk /var/lib/libvirt/images/global_logic/base_image.qcow2_1,device=disk \
+  --disk /var/lib/libvirt/images/global_logic/vm1,device=disk \
   --disk /var/lib/libvirt/images/global_logic/config_vm1.iso,device=cdrom \
   --os-type Linux \
   --os-variant ubuntu18.04 \
--- ansible/roles/vm_start/files/vm2.sh
+++ ansible/roles/vm_start/files/vm2.sh
@@ -3,7 +3,7 @@ virt-install \
   --memory 2048 \
   --vcpus 2 \
   --name worker2 \
-  --disk /var/lib/libvirt/images/global_logic/base_image.qcow2_2,device=disk \
+  --disk /var/lib/libvirt/images/global_logic/vm2,device=disk \
   --disk /var/lib/libvirt/images/global_logic/config_vm2.iso,device=cdrom \
   --os-type Linux \
   --os-variant ubuntu18.04 \
